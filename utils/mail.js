772c7d9e2136d0baa23580b7a04a21a1825d64fd
VE00AX107 <ve00ax107@globalymc.com>
2022-09-26 16:18:40 +0530

    test

--- utils/mail.js
+++ utils/mail.js
@@ -4,30 +4,30 @@ const db = require("./db");
 
 let transporter = undefined;
 let _mailConfig = undefined;
-const initializeTransport =
-  (transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: _mailConfig.user,
-      pass: _mailConfig.pass,
-    },
-  }));
+const initializeTransport = (transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: _mailConfig.user,
+    pass: _mailConfig.pass,
+  },
+}));
 
-const sendMail = async (subject, body) => { 
+const sendMail = async (subject, body) => {
   if (!!!transporter) {
     await db.connect();
     let _collection = await db.collection(constant.COLLLECTIONS.CONFIG);
     let _config = await _collection
       .find({ name: constant.CONFIG_TYPE.GMAIL })
       .toArray();
-      console.log('mail : '+JSON.stringify(_config));
+
+    console.log("mail : " + JSON.stringify(_config));
     _mailConfig = _config[0];
     initializeTransport();
   }
 
   let message = {
     from: _mailConfig.user,
-    to:_mailConfig.to,
+    to: _mailConfig.to,
     subject: subject,
     html: `<h1>${body}</h1>`,
   };
